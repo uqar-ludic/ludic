95a774f29e77001f6c55a7b23ad11bb4f1c5a92f
muggiwara <arnaud.billault@epitech.eu>
2014-02-19 18:13:03 -0500

    mise a jour de siteSandBox:
    ajout de la MVC Console servant a la demo de mardi

---
+++ Ludic/Gui/SiteSandBox/SiteSandBox/Controllers/ConsoleController.cs
@@ -0,0 +1,34 @@
+﻿using SiteSandBox.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SiteSandBox.Controllers
+{
+    public class ConsoleController : Controller
+    {
+        private SandBoxContext db = new SandBoxContext();
+
+        //
+        // GET: /Edition/
+
+        public ActionResult Edit(int id = 0)
+        {
+            ExerciceModel em = new ExerciceModel();
+            em.IdExercice = id;
+            return View(em);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ExerciceModel exercice)
+        {
+            return View(exercice);
+        }
+
+
+
+    }
+}
