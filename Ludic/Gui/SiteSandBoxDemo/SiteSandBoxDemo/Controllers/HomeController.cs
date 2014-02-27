402d307be3254c341a0d6e08cbbdd4a08ae300d7
muggiwara <arnaud.billault@epitech.eu>
2014-02-27 13:57:26 -0500

    Site Demo

---
+++ Ludic/Gui/SiteSandBoxDemo/SiteSandBoxDemo/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using SiteSandBox.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SiteSandBox.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private SandBoxContext db = new SandBoxContext();
+
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Bienvenu sur SandBox";
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
